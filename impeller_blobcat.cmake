7ffdbce24b2fb0fd0c1b3fb68fb48a259f79a6da
Brandon DeRosier <x@bdero.me>
2022-05-16 03:43:01 -0700

    Add functions to generate blobcat commands

--- impeller_blobcat.cmake
+++ impeller_blobcat.cmake
@@ -18,3 +18,40 @@ target_include_directories(blobcat
         $<BUILD_INTERFACE:${THIRD_PARTY_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}>) # For includes starting with "impeller/"
 target_link_libraries(blobcat PUBLIC impeller_blobcat_lib)
+
+# blobcat(OUTPUT filename INPUTS filename;filename ...)
+function(blobcat)
+    cmake_parse_arguments(ARG "" "OUTPUT" "INPUTS" ${ARGN})
+    blobcat_parse(CLI OUTPUT ${ARG_OUTPUT} INPUTS ${ARG_INPUTS} ${ARG_UNPARSED_ARGUMENTS})
+    get_filename_component(OUTDIR "${ARG_OUTPUT}" ABSOLUTE)
+    get_filename_component(OUTDIR "${OUTDIR}" DIRECTORY)
+    add_custom_command(OUTPUT ${ARG_OUTPUT}
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${OUTDIR}"
+        COMMAND "$<TARGET_FILE:blobcat>" ${CLI}
+        DEPENDS ${ARG_INPUTS}
+        COMMENT "Building blob ${ARG_OUTPUT}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+endfunction()
+
+# blobcat_parse(
+#    OUTPUT filename
+#    INPUTS filename;filename
+# )
+function(blobcat_parse CLI_OUT)
+    cmake_parse_arguments(ARG "" "OUTPUT" "INPUTS" ${ARGN})
+    set(CLI "")
+
+    # --output
+    if(ARG_OUTPUT)
+        list(APPEND CLI "--output" "${ARG_OUTPUT}")
+    endif()
+
+    # --input
+    if(ARG_INPUTS)
+        foreach(INPUT ${ARG_INPUTS})
+            list(APPEND CLI "--input" "${INPUT}")
+        endforeach()
+    endif()
+
+    set(${CLI_OUT} ${CLI} PARENT_SCOPE)
+endfunction()
