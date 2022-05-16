4e7d624d1237463b75b8d577a455174eb1e4b725
Brandon DeRosier <x@bdero.me>
2022-05-15 21:21:13 -0700

    Build shaderc from source

--- deps.sh
+++ deps.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-# Fetch git submodules.
+echo "Fetching git submodules..."
 git submodule update --init --recursive
+echo "Done fetching git submodules."
+echo
+
+ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd -P)
+
+echo "Fetching shaderc dependencies..."
+pushd $ROOT_DIR/third_party/shaderc > /dev/null
+  ./utils/git-sync-deps
+popd > /dev/null
+echo "Done fetching shaderc dependencies."
--- impellerc.cmake
+++ impellerc.cmake
@@ -18,27 +18,33 @@ if (WIN32)
          MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
     set_property(TARGET spirv-cross-msl PROPERTY
          MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+    set_property(TARGET shaderc PROPERTY
+         MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+    set_property(TARGET shaderc_util PROPERTY
+         MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
 endif()
 
-if(NOT IS_DIRECTORY ${SHADERC_INSTALL_DIR})
+if(NOT IS_DIRECTORY ${SHADERC_DIR})
     message(SEND_ERROR
       "Unable to configure the impellerc target because the shaderc install "
-      "directory (SHADERC_INSTALL_DIR) couldn't be found:"
-      "    ${SHADERC_INSTALL_DIR}\n"
-      "The shaderc build artifacts can be downloaded here:"
+      "directory (SHADERC_DIR) couldn't be found:"
+      "    ${SHADERC_DIR}\n"
+      "Run `deps.sh` to fetch dependencies.\n"
+      "The shaderc build artifacts can also be downloaded here:"
       "    https://github.com/google/shaderc/blob/main/downloads.md")
     return()
 endif()
 
-target_link_directories(impellerc PRIVATE ${SHADERC_INSTALL_DIR}/lib)
-target_include_directories(impellerc PRIVATE ${SHADERC_INSTALL_DIR}/include)
+#target_link_directories(impellerc PRIVATE ${SHADERC_INSTALL_DIR}/lib)
+#target_include_directories(impellerc PRIVATE ${SHADERC_INSTALL_DIR}/include)
 
 target_link_libraries(impellerc
     PRIVATE
-        fml impeller_base impeller_geometry spirv-cross-glsl spirv-cross-msl shaderc_combined)
+        fml impeller_base impeller_geometry spirv-cross-glsl spirv-cross-msl shaderc)
 target_include_directories(impellerc
     PRIVATE
         $<BUILD_INTERFACE:${THIRD_PARTY_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}> # For includes starting with "impeller/"
         $<BUILD_INTERFACE:${THIRD_PARTY_DIR}/inja/include> # For "inja/inja.hpp"
-        $<BUILD_INTERFACE:${THIRD_PARTY_DIR}/json/include>) # For "nlohmann/json.hpp"
+        $<BUILD_INTERFACE:${THIRD_PARTY_DIR}/json/include> # For "nlohmann/json.hpp"
+        $<BUILD_INTERFACE:${THIRD_PARTY_DIR}/shaderc/libshaderc/include>) # For "shaderc/shaderc.hpp"
