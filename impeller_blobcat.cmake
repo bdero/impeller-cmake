6a114dfce49cd1a41e25d0062af11b0adfe37007
Brandon DeRosier <x@bdero.me>
2022-05-21 05:00:38 -0700

    Make blobcat lib name consistent

--- impeller_blobcat.cmake
+++ impeller_blobcat.cmake
@@ -4,20 +4,20 @@ file(GLOB BLOBCAT_SOURCES ${BLOBCAT_DIR}/*.cc)
 list(FILTER BLOBCAT_SOURCES EXCLUDE REGEX ".*_unittests?\\.cc$")
 list(REMOVE_ITEM BLOBCAT_SOURCES "${BLOBCAT_DIR}/blobcat_main.cc")
 
-add_library(impeller_blobcat_lib STATIC ${BLOBCAT_SOURCES})
+add_library(impeller_blobcat STATIC ${BLOBCAT_SOURCES})
 
-target_include_directories(impeller_blobcat_lib
+target_include_directories(impeller_blobcat
     PUBLIC
         $<BUILD_INTERFACE:${THIRD_PARTY_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}>) # For includes starting with "impeller/"
-target_link_libraries(impeller_blobcat_lib PUBLIC fml impeller_base)
+target_link_libraries(impeller_blobcat PUBLIC fml impeller_base)
 
 add_executable(blobcat "${BLOBCAT_DIR}/blobcat_main.cc")
 target_include_directories(blobcat
     PUBLIC
         $<BUILD_INTERFACE:${THIRD_PARTY_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}>) # For includes starting with "impeller/"
-target_link_libraries(blobcat PUBLIC impeller_blobcat_lib)
+target_link_libraries(blobcat PUBLIC impeller_blobcat)
 
 # blobcat(OUTPUT filename INPUTS filename;filename ...)
 function(blobcat)
