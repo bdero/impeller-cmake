d2d23b2eaf0a379321661206d417ef14c22d2581
Ralph Thomas <ralphthomas@google.com>
2023-03-17 03:54:50 -0700

    Update to recent flutter HEAD

--- impeller_geometry.cmake
+++ impeller_geometry.cmake
@@ -3,12 +3,18 @@ set(IMPELLER_GEOMETRY_DIR ${FLUTTER_ENGINE_DIR}/impeller/geometry
 
 file(GLOB GEOMETRY_SOURCES ${IMPELLER_GEOMETRY_DIR}/*.cc)
 list(FILTER GEOMETRY_SOURCES EXCLUDE REGEX ".*_unittests?\\.cc$")
+list(FILTER GEOMETRY_SOURCES EXCLUDE REGEX ".*_benchmarks?\\.cc$")
 
 add_library(impeller_geometry STATIC ${GEOMETRY_SOURCES})
 
 target_include_directories(impeller_geometry
     PUBLIC
+        $<BUILD_INTERFACE:${THIRD_PARTY_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}>) # For includes starting with "impeller/"
 
 # TODO(bdero): Replace M_PI with kPi upstream.
 target_compile_definitions(impeller_geometry PRIVATE M_PI=kPi)
+
+target_link_libraries(impeller_geometry
+    PUBLIC
+        fml)
