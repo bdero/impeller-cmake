a07dcab019f35138921055e3c3c65e4cfbaf80e3
Zach Anderson <zra@google.com>
2023-04-14 07:31:35 -0700

    Add LICENSE and Flutter headers

--- cmake/impeller_core.cmake
+++ cmake/impeller_core.cmake
@@ -1,3 +1,7 @@
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 set(IMPELLER_CORE_DIR ${FLUTTER_ENGINE_DIR}/impeller/core
     CACHE STRING "Location of the Impeller core sources.")
 
--- cmake/libtess2.cmake
+++ cmake/libtess2.cmake
@@ -1,3 +1,7 @@
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 set(LIBTESS2_DIR ${THIRD_PARTY_DIR}/libtess2
     CACHE STRING "Location of the libtess2 sources.")
 
