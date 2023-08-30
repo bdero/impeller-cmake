f77d461c1b5ef7ee798ec76aa3263c194b78d3ea
Brandon DeRosier <bdero@google.com>
2023-08-29 17:55:20 -0700

    Update to engine head

--- cmake/impeller_typographer.cmake
+++ cmake/impeller_typographer.cmake
@@ -9,27 +9,20 @@ file(GLOB TYPOGRAPHER_SOURCES ${IMPELLER_TYPOGRAPHER_DIR}/*.cc)
 list(FILTER TYPOGRAPHER_SOURCES EXCLUDE REGEX ".*_unittests?\\.cc$")
 list(FILTER TYPOGRAPHER_SOURCES EXCLUDE REGEX ".*_playground?\\.cc$")
 
-# We always build the STB library, whether it's used or not.
-set(IMPELLER_CMAKE_STB_INCLUDE_DIR $<BUILD_INTERFACE:${THIRD_PARTY_DIR}>/stb)
-add_library(stb
+add_library(stb_truetype
     STATIC
-        ${IMPELLER_CMAKE_SRC_DIR}/typographer_backends/stb/stb_truetype.cc
-        ${IMPELLER_CMAKE_SRC_DIR}/typographer_backends/stb/stb_rect_packer.cc)
+        ${IMPELLER_CMAKE_SRC_DIR}/stb_truetype_impl.cc)
 
-target_include_directories(stb
+target_include_directories(stb_truetype
     PUBLIC
-        ${IMPELLER_CMAKE_STB_INCLUDE_DIR})
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>) # For includes starting with "third_party/stb/"
 
-if(${IMPELLER_CMAKE_TYPOGRAPHER_BACKEND_STB})
-    # STB based glyph atlas support.
-    file(GLOB TYPOGRAPHER_BACKEND_SOURCES
-        ${IMPELLER_CMAKE_SRC_DIR}/typographer_backends/stb/text_render_context_stb.cc
-        ${IMPELLER_CMAKE_SRC_DIR}/typographer_backends/stb/typeface_stb.cc)
-else()
-    # Dummy NOOP glyph atlas support.
-    file(GLOB TYPOGRAPHER_BACKEND_SOURCES
-        ${IMPELLER_CMAKE_SRC_DIR}/typographer_backends/noop/*.cc)
-endif()
+# Build the STB typographer backend by default.
+file(GLOB TYPOGRAPHER_BACKEND_SOURCES
+    ${IMPELLER_TYPOGRAPHER_DIR}/backends/stb/glyph_atlas_context_stb.cc
+    ${IMPELLER_TYPOGRAPHER_DIR}/backends/stb/text_frame_stb.cc
+    ${IMPELLER_TYPOGRAPHER_DIR}/backends/stb/typeface_stb.cc
+    ${IMPELLER_TYPOGRAPHER_DIR}/backends/stb/typographer_context_stb.cc)
 
 add_library(impeller_typographer
     STATIC
@@ -41,12 +34,12 @@ target_include_directories(impeller_typographer
         $<BUILD_INTERFACE:${IMPELLER_CMAKE_SRC_DIR}> # For includes starting with "typographer_backends/"
         $<BUILD_INTERFACE:${FLUTTER_INCLUDE_DIR}> # For includes starting with "flutter/"
         $<BUILD_INTERFACE:${FLUTTER_ENGINE_DIR}> # For includes starting with "impeller/"
-        $<BUILD_INTERFACE:${THIRD_PARTY_DIR}>/skia # Skia 
-        ${IMPELLER_CMAKE_STB_INCLUDE_DIR}) # stb includes.
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>) # For includes starting with "third_party/stb/"
 
 target_link_libraries(impeller_typographer
     PUBLIC
         fml
         impeller_base
         impeller_geometry
-        impeller_renderer)
+        impeller_renderer
+        stb_truetype)
